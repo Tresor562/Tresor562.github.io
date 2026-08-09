71a95f94aaec1a21384561431b48b41f4dfbe4cf
Trésor HONTONNOU <hontonnoutresor@gmail.com>
2026-08-09 16:24:33 +0100

    deploy: register native solo batch

--- nexus-store/play/native-games/loader.js
+++ nexus-store/play/native-games/loader.js
@@ -3,11 +3,15 @@ const loaders=new Map([
   [30,()=>import('./30-tic-tac-clash.js')],
   [42,()=>import('./42-memory-flip-clash.js')],
   [49,()=>import('./49-nail-pull-puzzle.js')],
+  [50,()=>import('./50-hoop-pup-challenge.js')],
+  [51,()=>import('./51-bee-dodge-garden.js')],
   [52,()=>import('./52-pop-it-fever.js')],
   [53,()=>import('./53-block-blast-quest.js')],
+  [54,()=>import('./54-night-alone-escape.js')],
   [55,()=>import('./55-color-sort-master.js')],
   [56,()=>import('./56-tower-stack-challenge.js')],
   [58,()=>import('./58-save-the-line-puzzle.js')],
+  [64,()=>import('./64-blow-it-up-chain.js')],
 ]);
 
 export const nativeGameIds=new Set(loaders.keys());
